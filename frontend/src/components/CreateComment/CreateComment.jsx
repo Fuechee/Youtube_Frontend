4dfc9e7a861e271c4cb3c4f60c6dd5e418d21f64
Fuechee <f9vang@gmail.com>
2022-11-19 20:27:00 -0600

    Created component CreateComment

---
+++ frontend/src/components/CreateComment/CreateComment.jsx
@@ -0,0 +1,51 @@
+const CreateComment = (props) => {
+    const [comment, setComment] = useState('');
+  
+    function handleSubmit(event){
+        event.preventDefault();
+        let newComment = {
+            user: props.userCode.user_id,
+            text: comment,
+            video_id: props.videoId,
+            likes: likes,
+            dislikes: dislikes
+        }
+        postComment(newComment);
+    }
+
+    async function postComment(comment){
+        const jwt = localStorage.getItem('token');
+        console.log(comment)
+        try {
+            let response = await axios.post('http://127.0.0.1:8000/api/comments/', comment, { headers: {Authorization: 'Bearer ' + jwt}});
+            console.log(response);
+            console.log(response.data);
+        }catch (ex) {
+            console.log(ex.response);
+        }
+        props.getComments();
+    }
+    return ( 
+        <Form onSubmit={handleSubmit}>
+        { localStorage.getItem('token') &&
+        <React.Fragment>
+        <Form.Label column sm="2"></Form.Label>
+        </React.Fragment>
+        }
+        { !localStorage.getItem('token') &&
+        <React.Fragment>
+        <Form.Label column sm="2">Sign in To Comment!</Form.Label>
+        </React.Fragment>
+        }
+        <Form.Group  className="mb-3" controlId="formPlaintextPassword">
+            <Form.Label column sm="2">Comments</Form.Label>
+                <Col sm="10">
+                    <Form.Control type="text" placeholder="Insert Comment Here" onChange={(event) => setComment(event.target.value)} />
+                </Col>
+            </Form.Group>
+        <Button type='submit'>submit</Button>
+        </Form>
+     );
+}
+ 
+export default CreateComment;
