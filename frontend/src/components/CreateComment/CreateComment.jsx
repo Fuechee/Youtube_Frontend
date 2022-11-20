279e514242c1ac0572981c0388f7d8f6b41fdef1
Fuechee <f9vang@gmail.com>
2022-11-20 17:29:45 -0600

    Changed some code in CreateComment

--- frontend/src/components/CreateComment/CreateComment.jsx
+++ frontend/src/components/CreateComment/CreateComment.jsx
@@ -1,55 +1,25 @@
-import axios from "axios";
-import React, {useState} from "react";
-import { Form, Col, Button } from 'react-bootstrap';
-
 const CreateComment = (props) => {
-    const [comment, setComment] = useState('');
-  
-    function handleSubmit(event){
-        event.preventDefault();
-        let newComment = {
-            user: props.userCode.user_id,
-            text: comment,
-            video_id: props.videoId,
-            likes: likes,
-            dislikes: dislikes
-        }
-        postComment(newComment);
-    }
-
-    async function postComment(comment){
-        const jwt = localStorage.getItem('token');
-        console.log(comment)
-        try {
-            let response = await axios.post('http://127.0.0.1:8000/api/comments/', comment, { headers: {Authorization: 'Bearer ' + jwt}});
-            console.log(response);
-            console.log(response.data);
-        }catch (ex) {
-            console.log(ex.response);
-        }
-        props.getComments();
-    }
-    return ( 
-        <Form onSubmit={handleSubmit}>
-        { localStorage.getItem('token') &&
-        <React.Fragment>
-        <Form.Label column sm="2"></Form.Label>
-        </React.Fragment>
-        }
-        { !localStorage.getItem('token') &&
-        <React.Fragment>
-        <Form.Label column sm="2">Sign in To Comment!</Form.Label>
-        </React.Fragment>
-        }
-        <Form.Group  className="mb-3" controlId="formPlaintextPassword">
-            <Form.Label column sm="2">Comments</Form.Label>
-                <Col sm="10">
-                    <Form.Control type="text" placeholder="Insert Comment Here" onChange={(event) => setComment(event.target.value)} />
-                </Col>
-            </Form.Group>
-        <Button type='submit'>submit</Button>
-        </Form>
-     );
+    <div>
+    <form className="form-inline" onSubmit={(e) => props.handleSubmit(e)}>
+      <div class="form-group mx-sm-3 mb-2">
+        <input
+          type="text"
+          // value={props.comment}
+          placeholder="Comment"
+          id="commentInputField"
+          onChange={(e) => props.setNewComment(e.target.value)}
+        />
+        {` `}
+        <button
+          className="btn btn-dark"
+          type="submit"
+          id="submitCommentButton"
+        >
+          Create
+        </button>
+      </div>
+    </form>
+  </div>
 }
  
 export default CreateComment;
